b117089c9c49dbd0b1359b73378d162678181666
Padilla <Padilla@DESKTOP-SP5IBFR>
2023-11-10 21:38:41 -0600

    Agregar archivos de proyecto.

---
+++ Models/Personas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PM2_Tarea1_3.Models
+{
+    public class Personas
+    {
+        public int Id { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public int Edad { get; set; }
+        public string Correo { get; set; }
+        public string Direccion { get; set; }
+    }
+}
---
+++ Services/Dbpersonas.cs
@@ -0,0 +1,16 @@
+﻿// Services/DatabaseService.cs
+using Microsoft.EntityFrameworkCore;
+using PM2_Tarea1_3.Models;
+using System.IO;
+using Xamarin.Essentials;
+
+public class DatabaseService : DbContext
+{
+    public DbSet<Personas> Personas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string dbPath = Path.Combine(FileSystem.AppDataDirectory, "miBaseDeDatos.db3");
+        optionsBuilder.UseSqlite($"Data Source={dbPath}");
+    }
+}
